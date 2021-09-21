94000a08054c74abc24fdee789ac75b890584604
James Torres <james.torres01@gmail.com>
2021-09-21 17:20:19 -0400

    adding files to git

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import {BrowserRouter, Route}  from 'react-router-dom';
+import Photos from "./components/Photos"
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1> Pick a Pic!</h1>
+      <BrowserRouter>
+        <div>
+          <Route component={Photos} path="/" />
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Photos.js
@@ -0,0 +1,39 @@
+import React, {useState, useEffect} from 'react';
+import Heart from "react-animated-heart";
+
+export default function Photos() {
+
+    const [photoData, setPhoto] = useState([]);
+    
+    useEffect(() => {
+        fetchPhoto();
+
+        async function fetchPhoto() {
+            const res = await fetch("https://api.nasa.gov/planetary/apod?api_key=K2qpEA9Sghudf46QgREROngHYEarQhJKmzO95ChK");
+
+            const data = await res.json();
+            setPhoto(data);
+            console.log(data);
+        }
+       
+}, []);
+
+    const [isClick, setClick] = useState(false);
+
+
+    return (
+     
+     <div>
+          <img src= {photoData.url} />
+        <div>
+            <h2>{photoData.title}</h2>
+            <h4>{photoData.date}</h4>
+            <p>{photoData.explanation}</p>
+        </div>
+        <Heart isClick={isClick} onClick={() => setClick(!isClick)} />    </div>
+       
+    
+    )
+
+}
+
